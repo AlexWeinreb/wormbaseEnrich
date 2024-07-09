5e44bedecf0ae304487bec48c8561ff421445fd2
AlexWeinreb <alexis.weinreb@yale.edu>
2024-07-08 20:33:52 -0400

    Remove python dependency, reimplement fetch and enrich

--- R/enrichment_analysis.R
+++ R/enrichment_analysis.R
@@ -1,32 +1,130 @@
 #' Execute complete enrichment analysis (hypergeometric test, BH correction).
 #'
 #'
-#' @param gene_list a list of non-redundant Wormbase IDs.
+#' @param gene_list a list of Wormbase IDs.
 #' @param dictionary dictionary created with `fetch_dictionary()`.
-#' @param alpha significance threshold, defaults to 0.05.
-#' @param filename if not NULL, file name to save results as a csv.
+#' @param alpha significance threshold.
 #'
 #'
 #' @returns a data.frame containing significantly enriched tissues
 #'
 #' @export
-enrichment_analysis <- function(gene_list, tissue_df, alpha = 0.05, filename = NULL) {
-  if(! is.null(filename)){
-    if(!(is.character(filename) && nchar(filename) > 0)){
-      stop("If filename is not NULL, it should be a valid file name.")
-    }
-    save <- TRUE
-  } else{
-    save <- FALSE
-    filename <- ""
+enrichment_analysis <- function(gene_list, dictionary, alpha = 0.05) {
+
+
+  if(!all(gene_list %in% dictionary$wbid)){
+    ids_not_found <- gene_list[! gene_list %in% dictionary$wbid]
+    stop(length(ids_not_found), " gene ID not found: ", paste0(head(ids_not_found), collapse = ", "))
   }
-  tea$enrichment_analysis(
-    gene_list = gene_list,
-    tissue_df = tissue_df,
-    alpha = alpha,
-    aname = filename,
-    save = save,
-    show = FALSE
-  )
+
+  if(anyDuplicated(gene_list) > 0){
+    warning("Duplicate entries in gene_list were removed")
+    gene_list <- unique(gene_list)
+  }
+
+
+  nb_terms <- ncol(dictionary) - 1L
+
+  res <- tibble::tibble( term = names(dictionary[-1]) ) |>
+    tidyr::separate_wider_regex(term,
+                                patterns = c(term_name = "[[:print:]]+",
+                                             " ",
+                                             term_id = "(?:WBbt|WBPhenotype|GO)\\:[0-9]{7}$"))
+
+
+
+  # q: nb of white (in tissue) balls picked (in list)
+  # m: nb of white balls: nb of genes in tissue
+  # n: nb of black balls: nb of genes not in tissue
+  # k: nb of balls drawn: nb of genes in list
+  #
+  # Note, following the Python version, we count a gene each time it appears
+  # in a tissue, so the same gene is counted several times.
+  # From paper: "Although a user will input gene IDs, we test the number of occurrences
+  # of a term within the gene list, so a single gene can contribute to multiple terms"
+
+  # tot_not_in_list <- setdiff(dictionary$wbid, gene_list) |> length()
+  # tot_in_ref <- sum(ref)
+
+  nb_total <- sum(dict[-1])
+  nb_listed_in_all_tissues <- sum(dict[dict$wbid %in% gene_list, -1])
+
+
+  res$expected <- vapply(dictionary[-1],
+                         \(ref){
+                           in_tissue <- sum(ref)
+                           # mean = k*m/(m+n)
+                           nb_listed_in_all_tissues*in_tissue/(nb_total)
+
+                         },
+                         FUN.VALUE = double(1L))
+
+
+
+
+  res$observed <- vapply(dictionary[-1],
+                         \(ref){
+                           intersect(gene_list, dictionary$wbid[ref == 1L]) |> length()
+
+                         },
+                         FUN.VALUE = integer(1L))
+
+  res$enrichment_fc <- res$observed / res$expected
+
+
+  res$p_value <- vapply(dictionary[-1],
+                        \(ref){
+
+                          in_ref_and_list <- intersect(gene_list, dictionary$wbid[ref == 1L]) |> length()
+                          in_tissue <- sum(ref)
+
+                          phyper(q = in_ref_and_list,
+                                 m = in_tissue,
+                                 n = nb_total - in_tissue,
+                                 k = nb_listed_in_all_tissues,
+                                 lower.tail = FALSE)
+                        },
+                        FUN.VALUE = double(1L))
+
+
+
+  res$q_value <- stats::p.adjust(res$p_value, method = "BH")
+  res
 }
+
+res |> filter(paste(term_name, term_id) %in% res_py2$term) |> as.data.frame()
+
+
+term_full <- names(dictionary[-1])
+reg
+
+
+
+stringr::str_detect(res$term[1:3],
+                    pattern = "[a-zA-Z0-9]+ (?:WBbt|WBPhenotype|GO)\\:[0-9]{7}")
+
+dict_go |>
+  tidyr::pivot_longer(-wbid,
+                      names_to = "term") |>
+  dplyr::select(-c(wbid, value)) |>
+  unique() |>
+  tidyr::separate_wider_regex(term,
+                              patterns = c(term2 = "[[:print:]]+", " ",
+                                           term_id = "(?:WBbt|WBPhenotype|GO)\\:[0-9]{7}$"))
+res |>
+  tidyr::separate_wider_regex(term,
+                              patterns = c(term2 = "[[:print:]]+", " ",
+                                           term_id = "(?:WBbt|WBPhenotype|GO)\\:[0-9]{7}$"))
+
+
+
+
+
+
+
+
+
+
+
+
 
--- R/fetch_dictionary.R
+++ R/fetch_dictionary.R
@@ -1,20 +1,32 @@
 #' Fetch a dictionary
 #'
-#' If `analysis` isn't specified, fetches the tissue dictionary.
+#' Download a dictionary file from Wormbase
 #'
 #'
-#' @param analysis the type of analysis (tissue, phenotype, Gene Ontology)
+#' @param analysis the type of analysis (tissue, phenotype, go)
 #'
-#' @section Output:
-#' A dataframe containing the dictionary of interest
+#' @returns A data.frame containing the dictionary of interest, one row per gene, one column per term
 #'
 #' @export
 fetch_dictionary <- function(analysis = c("tissue", "phenotype", "go")) {
   analysis <- tolower(analysis)
+  if(length(analysis) != 1L){
+    stop("Only provide one type of analysis")
+  }
   analysis <- match.arg(analysis)
-  tea$fetch_dictionary(
-    analysis = analysis
+
+
+
+  url_base = 'http://caltech.wormbase.org/TissueEnrichmentAnalysis/'
+
+  filename <- switch (analysis,
+    "tissue" = "anatomy_dict.csv",
+    "phenotype" = "phenotype_dict.csv",
+    "go" = "go_dict.csv"
   )
+
+  read.csv(paste0(url_base, filename), check.names = FALSE)
+
 }
 
 
